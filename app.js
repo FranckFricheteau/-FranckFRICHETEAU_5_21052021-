300a83266fca5096a74dcae9d05a4bddcdb12905
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-05-23 16:55:57 +0200

    mise à jour app.js

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 const express = require('express');
 
-//body-parser
-const bodyParser = require('body-parser');
-
 //MongoDB object modeling tool
 const mongoose = require('mongoose');
-
-//pour accéder au path de notre serveur
-const path = require('path');
-
-//import des routeurs dans l'application
-const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
-const sauce = require('./models/sauce');
 
 //mongodb authentification
 
@@ -23,20 +12,37 @@ mongoose.connect('mongodb+srv://new-user-1:y6q7eeUXStMh6nt@cluster0.bsxcf.mongod
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//pour accéder au path de notre serveur
+const path = require('path');
+
+//import des routeurs dans l'application
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
 //pour créer une application express
 const app = express();
 
+//Rate Limit
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes 
+    max: 100 // nb dessaie 100
+});
+
+app.use("/api/auth/login", limiter);
+
+
 //CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_APP_URL);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-//middleware body-parser (deprecated)
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 //gestionnaire de routage pour les images
 //__dirname: le dossier où l'on se trouve
@@ -44,7 +50,9 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 //enregistrement des routes
 app.use('/api/auth', userRoutes);
-app.use('/api/sauces', sauceRoutes);
+
+app.use('/api', sauceRoutes);
+
 
 //exporter cette application pour y accéder depuis les autres fichiers notamment le serveur
 module.exports = app;
