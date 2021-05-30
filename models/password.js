3095f6c8ff0c00ac88d7c55d34b2c82dc1eefcf3
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-05-30 22:46:46 +0200

    insert password.js

---
+++ models/password.js
@@ -0,0 +1,17 @@
+var passwordValidator = require('password-validator');
+
+// Créer un schema
+var passwordSchema = new passwordValidator();
+
+// Ajouter des propriétés, restrictions
+passwordSchema
+    .is().min(8) // Minimum 8 caractères
+    .is().max(100) // Maximum 100 caractères
+    .has().uppercase() // Doit contenir au moins une majuscule
+    .has().lowercase() // Doit contenir au moins une minuscule
+    .has().digits(2) // Contient au moins 2 chiffres
+    .has().not().spaces() // Ne contient pas d'espaces
+    .is().not().oneOf(['Passw0rd', 'Password123']); // Intrerdit ou bloque ce type de mots de passe rentré par l'user
+
+
+module.exports = passwordSchema;
