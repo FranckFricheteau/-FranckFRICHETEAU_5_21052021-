bb94ad89c48ee376105d0e45e20b339d217481bc
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-05-30 22:55:10 +0200

    update sauce.js

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce');
-
-//Importer le Middleware de securiter auth
-const auth = require('../middleware/auth');
+//Importer le Middleware de securité auth
+const auth = require('../middleware/auth')
 
 //Importer multer
 const multer = require('../middleware/multer-config');
 
-router.get('/', auth, sauceCtrl.findSauces); //récupérer toutes les sauces
-router.get('/:id', auth, sauceCtrl.findOneSauce) //récupérer une sauce
-router.post('', auth, multer, sauceCtrl.createSauce); //créer une sauce
-router.put('/:id', auth, multer, sauceCtrl.modifySauce); //modifier une sauce
+//Importer controllers sauce
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', auth, sauceCtrl.findSauces) // trouver une sauce
+router.get('/:id', auth, sauceCtrl.findOneSauce) // trouver toutes les sauces
+router.post('', auth, multer, sauceCtrl.createSauce); // créer une sauce
+router.put('/:id', auth, multer, sauceCtrl.modifySauce); // modifier une sauce
 router.delete('/:id', auth, sauceCtrl.deleteSauce); // supprimer une sauce
 router.post('/:id/like', auth, sauceCtrl.evaluateSauce); // noter une sauce
-
-//Like Dislake de sauce
-router.post('/sauces/:id/like', auth, multer, sauceCtrl.likeSauce);
 
 module.exports = router;
