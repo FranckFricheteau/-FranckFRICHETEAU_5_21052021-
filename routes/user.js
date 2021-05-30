ce61f2246925a61703b3161cd254d3f9bb472d85
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-05-30 22:56:22 +0200

    update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,10 @@
-// express pour crée un router
 const express = require('express');
-// Declarer le router
 const router = express.Router();
 
-// associer les fonction aux differente routes
 const userCtrl = require('../controllers/user');
+const passwordValidator = require('../middleware/passwordValidator');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
-//exporter le router
 module.exports = router;
