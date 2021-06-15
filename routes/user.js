61a622c917a29d88022707fee98153be30e0d9b0
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-06-15 16:44:27 +0200

    update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+//Importation du controller user
 const userCtrl = require('../controllers/user');
+
+//importation du middleware passwordValidator
 const passwordValidator = require('../middleware/passwordValidator');
 
+//importation des routes signup & login
 router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
