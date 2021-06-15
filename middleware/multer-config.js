db1802e1d3c9b82782edbabbd7612da6071b9664
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-06-15 16:37:49 +0200

    update multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -10,24 +10,23 @@ const MIME_TYPES = {
 
 
 //Crée un objet de configuration pour multer
-//la fonction diskStorage de multer dit qu'on enregistre sur le disque
+//la fonction diskStorage de multer enregistre sur le disque
 const storage = multer.diskStorage({
-    // destination va expliquer a multer dans quel dossier enregistrer les fichiers c'est une fonction qui prend 3 arguments, req fil et callback
+    // destination va expliquer a multer dans quel dossier enregistrer les fichiers c'est une fonction qui prend 3 arguments, req file et callback
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
-    //le deuxieme argument est filename cela explique quel nom est utilisé a multer donc fonction a 3 argument
+    //le deuxieme argument est filename cela explique quel nom est utilisé a multer donc fonction a 3 arguments
     filename: (req, file, callback) => {
-        //Il est possible davoir des espace dans le nom du fichier donc on elimine les espaces et on les remplacent par des _ avec la methode split et .join
+        //Il est possible davoir des espaces dans le nom du fichier, élimination des espaces et sont remplacés par des _ avec la methode split et .join
         const name = file.originalname.split(' ').join('_');
-        //Appliquez une extension au fichier donc retour a la ligne 6 pour crée les extensions
+        //Appliquer une extension au fichier donc retour a la ligne 6 pour crée les extensions
         const extension = MIME_TYPES[file.mimetype];
 
-        //On appel le callback, et on crée le file name entier donc le name du dessus + date.now + un point + l'extension sdu fichier
+        //Appel de la callback, création du file name entier donc le name du dessus + date.now + un point + l'extension sdu fichier
         callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-//On exporte notre middleware multer completmeent configuré, on appel multer on place notre objet storage et apel la methode single
-// pour dire quil s'agit d'un fichier unique et on lui dit qui s'agit de fichier images uniquement
+// Fichier unique, fichier images uniquement
 module.exports = multer({ storage: storage }).single('image');
