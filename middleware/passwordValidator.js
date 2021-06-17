034fb2ac8821d4f5bf7395257d57afae8234d13d
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-06-17 10:44:28 +0200

    update passwordValidator.js

--- middleware/passwordValidator.js
+++ middleware/passwordValidator.js
@@ -1,7 +1,8 @@
+//Importation du model password
 const passwordSchema = require('../models/password');
 
 module.exports = (req, res, next) => {
-    if (!passwordSchema.validate(req.body.password)) { //sécurité de mot de passe suivant les règles définies ci-dessous
+    if (!passwordSchema.validate(req.body.password)) { //si mot de passe entré par l'utilisateur différent de celui attendu par la règle de sécurité, affiche un message d'erreur 
         return res.status(400).json({ error: 'Le mot de passe doit contenir entre 8 et 100 caractères, contenant au moins une majuscule, une minuscule et deux chiffres sans espaces' });
     }
     next();
