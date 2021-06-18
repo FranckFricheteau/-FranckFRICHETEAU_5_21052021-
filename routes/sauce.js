fe4a0e13c8291e4d696a2fd42e2f22dfaf477d06
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-06-18 11:18:54 +0200

    update sauce.js

--- routes/sauce.js
+++ routes/sauce.js
@@ -13,11 +13,11 @@ const multer = require('../middleware/multer-config');
 //Importer controllers sauce
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', auth, sauceCtrl.findSauces) // trouver toutes les sauces
-router.get('/:id', auth, sauceCtrl.findOneSauce) // trouver une sauce
-router.post('', auth, multer, sauceCtrl.createSauce); // créer une sauce
-router.put('/:id', auth, multer, sauceCtrl.modifySauce); // modifier une sauce
-router.delete('/:id', auth, sauceCtrl.deleteSauce); // supprimer une sauce
-router.post('/:id/like', auth, sauceCtrl.evaluateSauce); // noter une sauce
+router.get('/', auth, sauceCtrl.findSauces) // Renvoie le tableau de toutes les sauces dans la base de données
+router.get('/:id', auth, sauceCtrl.findOneSauce) // Renvoie la sauce avec l'ID fourni
+router.post('', auth, multer, sauceCtrl.createSauce); // Capture et enregistre l'image, analyse la sauce en utilisant une chaîne de caractères et l'enregistre dans la base de données, en définissant correctement son image URL. 
+router.put('/:id', auth, multer, sauceCtrl.modifySauce); // Met à jour la sauce avec l'identifiant fourni
+router.delete('/:id', auth, sauceCtrl.deleteSauce); // Supprime la sauce avec l'ID fourni
+router.post('/:id/like', auth, sauceCtrl.evaluateSauce); // Définit le statut "j'aime" pour userID fourni. Si j'aime = 1, l'utilisateur aime la sauce. Si j'aime = 0, l'utilisateur annule ce qu'il aime ou ce qu'il n'aime pas. Si j'aime =-1, l'utilisateur n'aime pas la sauce.
 
 module.exports = router;
