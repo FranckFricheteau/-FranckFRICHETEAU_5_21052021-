54cd5dc5f234f75886ca1a9a620bdbce35fe4805
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-09-13 17:04:19 +0200

    update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,150 +1,50 @@
-const bcrypt = require('bcrypt'); // Importation du package de chiffrement bcrytp
-const jwt = require('jsonwebtoken'); // Importation du package jsonwebtoken
-const User = require('../models/user'); // Importation modèle User
-const fs = require('fs'); // 
-const Utils = require('../libs/utils.js');
+const User = require('../models/user');
 
-// Inscription pour enregistrer des nouveaux utilisateurs
+const jwt = require('jsonwebtoken'); //Il permet l'échange sécurisé de jetons (tokens) entre plusieurs parties
+
+const bcrypt = require('bcrypt'); // fonction de hachage de mot de passe
+
+
+//pour enregistrer des nouveaux utilisateurs
 exports.signup = (req, res, next) => {
 
-    bcrypt.hash(req.body.password, 10)
+    bcrypt.hash(req.body.password, 10) // Hachage du mot passe avec 10 tours d'algorithmes de hachage
         .then(hash => {
             const user = new User({
-                id: 1,
-                pseudo: req.body.pseudo,
                 email: req.body.email,
-                password: hash,
-                profilPic: `profile_utilisator.png`,
-                isAdmin: false,
-                isActive: true
+                password: hash
             });
-            console.log(user);
-
-            User.create(user, (err, data) => {
-                if (err) {
-                    return res.status(400).json({ message: 'Impossible de créer l\'utilisateur, déjà existant!' });
-                }
-                res.send(data);
-            })
-
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' })) // Création utilisateur, requête réussie et ressource créée code 201 OK
+                .catch(error => res.status(400).send('Utilisateur déjà existant !')); // Erreur 400 Utilisateur déjà existant
         })
-
-    .catch(error => res.status(500).json({ error: 'le serveur a rencontré un problème inattendu empêchant de répondre à la requête' }));
-
+        .catch(error => res.status(500).json({ error: 'le serveur a rencontré un problème inattendu empêchant de répondre à la requête' })); // erreur serveur code 500
 };
 
-// Connexion
+//Connexion des utilisateurs existants
 exports.login = (req, res, next) => {
-    User.findOneByEmail(req.body.email, (err, result) => {
-
-        if (!result) {
-            return res.status(400).json({ message: 'Utilisateur non trouvé ou désactivé' });
-        }
-
-        bcrypt.compare(req.body.password, result.password)
-            .then(valid => {
-                if (!valid) {
-                    return res.status(401).json({ message: 'Mot de passe invalide' })
-                } else {
-                    let payload = {
-                        'userId': result.id,
-                        'isAdmin': !!result.isAdmin
-                    };
-                    let profile = result.profilPic;
-                    if (!result.profilPic) {
-                        profile = ''
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(400).send('Adresse mail inexistante !'); //Erreur adresse mail introuvable code 400
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(400).send('Mot de passe incorrect !'); //Erreur mot de passe invalide code 400
                     }
-                    res.status(200).json({
-                        pseudo: result.pseudo,
-                        userId: result.id,
-                        profilPic: profile,
-                        isAdmin: result.isAdmin,
-                        isActive: result.isActive,
-                        token: jwt.sign(
-                            payload,
-                            'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }
+                    res.status(200).json({ // l'user est bien connecté code 200 OK
+                        userId: user._id,
+                        token: jwt.sign({ userId: user._id },
+                            //clé secrète pour l'encodage 
+                            'RANDOM_TOKEN_SECRET',
+                            //chaque token durera 24 h
+                            { expiresIn: '24h' }
                         )
-                    })
-                }
-            })
-            .catch(error => res.status(500).json({ error: "Erreur serveur" }));
-    })
-};
-
-// Récupérer tous les utilisateurs
-exports.getAllUsers = (req, res, next) => {
-    User.findAll((err, result) => {
-        if (err) {
-            return res.status(404).json({ message: 'Utilisateurs non trouvés' });
-        } else {
-            res.status(200).json(result)
-        }
-    })
-};
-
-// Réupérer un seul user
-exports.getOneUser = (req, res, next) => {
-    let id = req.body.userId
-    User.findOneById(id, (err, result) => {
-        if (err) {
-            return res.status(404).json({ message: 'Utilisateur non trouvé' });
-        } else {
-            res.status(200).json(result)
-        }
-    })
-};
-
-// Mofifier un pseudo
-exports.updateOneUserPseudo = (req, res, next) => {
-    let myToken = Utils.getReqToken(req);
-    // check pas d'usurpation de user_id
-    if ((!myToken.isAdmin) && (myToken.userId != req.params.id)) {
-        return res.status(401).json({ message: 'Non authorisé' });
-    }
-    let user = {
-        'id': req.params.id,
-        'pseudo': req.body.pseudo,
-    }
-    User.modifyPseudo(user, (err, result) => {
-        if (err) {
-            return res.status(400).json({ message: 'Modification non effectuée' });
-        }
-        res.status(201).json({
-            pseudo: result.pseudo
+                    });
+                })
+                //si problème de connexion à mongodb
+                .catch(error => res.status(500).json({ error: 'le serveur a rencontré un problème inattendu empêchant de répondre à la requête' }));
         })
-    })
-};
 
-// Mofifier une profilPic
-exports.updateOneUserFile = (req, res, next) => {
-    let myToken = Utils.getReqToken(req);
-    // check pas d'usurpation de user_id
-    if ((!myToken.isAdmin) && (myToken.userId != req.params.id)) {
-        return res.status(401).json({ message: 'Non authorisé' });
-    }
-
-    let user = {
-        'id': req.params.id,
-        'profilPic': req.file ? req.file.filename : null,
-    }
-    User.modifyProfilPic(user, (err, result) => {
-        if (err) {
-            return res.status(400).json({ message: 'BACK Modification non effectuée' });
-        }
-        res.status(201).json({
-            profilPic: req.file.filename,
-        });
-    });
-};
-
-// Supprimer un user
-exports.deactivateUser = (req, res, next) => {
-    User.deactivate(req.params.id, (err, result) => {
-        if (err) {
-            return res.status(400).json({ message: 'Impossible de supprimer l\'utilisateur' });
-        }
-        res.status(204).json({
-            message: 'Utilisateur correctement supprimé'
-        })
-    })
 };
